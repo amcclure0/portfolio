e335a0ada92898dcbf635873e1bd2abe11eed72e
Adrian McClure <106691037+amcclure0@users.noreply.github.com>
2024-06-17 15:31:19 -0500

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid.shared import JsCode
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "link": [
+        "https://example.com/alice",
+        "https://example.com/bob",
+        "https://example.com/charlie",
+    ],
+}
+
+df = pd.DataFrame(data)
+gb = GridOptionsBuilder.from_dataframe(df,
+                                        editable=True)
+
+cell_renderer =  JsCode("""
+                        function(params) {return `<a href=${params.value} target="_blank">${params.value}</a>`}
+                        """)
+
+
+gb.configure_column(
+    "link",
+    headerName="link",
+    width=100,
+    cellRenderer=JsCode("""
+        class UrlCellRenderer {
+          init(params) {
+            this.eGui = document.createElement('a');
+            this.eGui.innerText = 'SomeText';
+            this.eGui.setAttribute('href', params.value);
+            this.eGui.setAttribute('style', "text-decoration:none");
+            this.eGui.setAttribute('target', "_blank");
+          }
+          getGui() {
+            return this.eGui;
+          }
+        }
+    """)
+)
+
+grid = AgGrid(df,
+            gridOptions=gb.build(),
+            updateMode=GridUpdateMode.VALUE_CHANGED,
+            allow_unsafe_jscode=True)
