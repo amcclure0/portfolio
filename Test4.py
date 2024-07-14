467c8ba82531f4c6e8474efeca6babe503a597f5
Adrian McClure <106691037+amcclure0@users.noreply.github.com>
2024-07-14 16:21:40 -0500

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -33,11 +33,11 @@ custom_html = """
         width: 120%;
         height: 150px;
         overflow: hidden;
-        object-fit: fill;
+        object-fit: visible;
     }
     .banner img {
         width: 120%;
-        object-fit: fill;
+        object-fit: visible;
     }
 </style>
 """
@@ -46,7 +46,7 @@ st.title("Welcome to Flight Finder")
 st.markdown("Did you know that not all connecting flight itineraries are searchable on popular travel search engines like Google Flights? Flight Finder finds flight itineraries not marketed by the airlines or on Google Flights.  Whether you're dealing with a cancelled flight and need to find all options to get to your destination fast or are just looking for better prices or times Flight Finder can help.")
 left, right = st.columns([2,4])
 right.image('https://github.com/amcclure0/portfolio/blob/main/Boeing%20777%20Background%20(reduced).png?raw=true', width = 900)
-left.video('https://www.youtube.com/watch?v=1-m_Jjse-cs', format="video/mp4", start_time=706, subtitles=None, end_time=None, loop=True, autoplay=False, muted=False)
+# left.video('https://www.youtube.com/watch?v=1-m_Jjse-cs', format="video/mp4", start_time=706, subtitles=None, end_time=None, loop=True, autoplay=False, muted=False)
 
 ###establishing page formatting
 st.markdown(
