53645ddc4f612d1ff0e05d692ec04b84b08bf200
Adrian McClure <106691037+amcclure0@users.noreply.github.com>
2024-07-14 14:50:03 -0500

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -25,7 +25,7 @@ st.markdown(
 #         background: url('https://github.com/amcclure0/portfolio/blob/main/Boeing%20777%20Background.png?raw=true');
 #         background-size: cover;
 #     }
-      [data-testid=stAppViewContainer {
+      .appview-container.main.block-container {
          padding-top: 0rem;
       }
    </style>
