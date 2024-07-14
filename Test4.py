f2280caf82d33981cb3509f6202e2b153ec2e69a
Adrian McClure <106691037+amcclure0@users.noreply.github.com>
2024-07-14 14:54:16 -0500

    Update Test4.py

--- Test4.py
+++ Test4.py
@@ -25,7 +25,7 @@ st.markdown(
 #         background: url('https://github.com/amcclure0/portfolio/blob/main/Boeing%20777%20Background.png?raw=true');
 #         background-size: cover;
 #     }
-      root.stApp stAppEmbeddingId-sgmpsg1l0ju7 streamlit-wide st-emotion-cache-1r4qj8v erw9t6i1.appview-container.block-container {
+      root.stApp stApp.stHeader.appview-container.stAppViewContainer.stAppViewBlockContainer {
          padding-top: 0rem;
       }
    </style>
